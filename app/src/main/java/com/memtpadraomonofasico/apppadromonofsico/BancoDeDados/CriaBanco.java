c9f750e0a947e446b81309902ff33b74bde6abc9
MEMT\loraine.duarte <loraine.duarte@memt.com.br>
2018-02-09 19:48:00 -0200

    Criação de tabela de medidor

--- app/src/main/java/com/memtpadraomonofasico/apppadromonofsico/BancoDeDados/CriaBanco.java
+++ app/src/main/java/com/memtpadraomonofasico/apppadromonofsico/BancoDeDados/CriaBanco.java
@@ -11,11 +11,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class CriaBanco extends SQLiteOpenHelper {
 
     private static final String NOME_BANCO = "banco.db";
-    public static final String TABELA = "avaliador";
-    public static final String ID = "_id";
+    private static final int VERSAO = 1;
+    //tabela de avaliador
+    public static final String TABELA_AVALIADOR = "avaliador";
+    public static final String ID_AVALIADOR = "_id_avaliador";
     public static final String NOME_AVALIADOR = "avaliador_nome";
     public static final String MATRICULA = "avaliador_matricula";
-    private static final int VERSAO = 1;
+    //tabela de medidor
+    public static final String TABELA_MEDIDOR = "medidor";
+    public static final String ID_MEDIDOR = "_id_medidor";
+    public static final String NUM_SERIE = "medidor_num_serie";
+    public static final String FABRICANTE = "medidor_fabricante";
+    public static final String NUM_ELEMENTOS = "medidor_num_elementos";
+    public static final String MODELO = "medidor_modelo";
+    public static final String CORRENTE_NOMINAL = "medidor_corrente_nominal";
+    public static final String CLASSE = "medidor_classe";
+    public static final String ANO_FABRICACAO = "medidor_ano_fabricacao";
+    public static final String TENSAO_NOMINAL = "medidor_tensao_nominal";
+    public static final String KDKE = "medidor_KdKe";
+    public static final String PORT_INMETRO = "medidor_port_inmetro";
+
 
     public CriaBanco(Context context){
         super(context, NOME_BANCO,null,VERSAO);
@@ -23,19 +38,35 @@ public class CriaBanco extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String sql = "CREATE TABLE IF NOT EXISTS "+TABELA+" (  "
-                + ID + " INTEGER PRIMARY KEY AUTOINCREMENT , "
+        String sqlAvaliador = "CREATE TABLE IF NOT EXISTS "+TABELA_AVALIADOR+" (  "
+                + ID_AVALIADOR + " INTEGER PRIMARY KEY AUTOINCREMENT , "
                 + NOME_AVALIADOR + " text, "
                 + MATRICULA + " text "
                 +" )";
-        db.execSQL(sql);
+        db.execSQL(sqlAvaliador);
+
+        String sqlMedidor = "CREATE TABLE IF NOT EXISTS "+TABELA_MEDIDOR+" (  "
+                + ID_MEDIDOR + " INTEGER PRIMARY KEY AUTOINCREMENT , "
+                + TABELA_MEDIDOR + " text, "
+                + ID_MEDIDOR + " text, "
+                + NUM_SERIE + " text, "
+                + FABRICANTE + " text, "
+                + NUM_ELEMENTOS + " text, "
+                + MODELO + " text, "
+                + CORRENTE_NOMINAL + " text, "
+                + CLASSE + " text, "
+                + ANO_FABRICACAO + " text, "
+                + TENSAO_NOMINAL + " text, "
+                + KDKE + " text, "
+                + PORT_INMETRO + " text "
+                +" )";
+        db.execSQL(sqlMedidor);
     }
-
-
-
+    
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(" DROP TABLE IF EXISTS " + TABELA);
+        db.execSQL(" DROP TABLE IF EXISTS " + TABELA_AVALIADOR);
+        db.execSQL(" DROP TABLE IF EXISTS " + TABELA_MEDIDOR);
         onCreate(db);
     }
 }
